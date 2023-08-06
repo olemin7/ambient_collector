4fe7457fb4536b9d48ad4e5f74eafc093fc6507f
Oleksandr Minenko <oleksandr.minenko@ringteam.com>
2023-08-06 19:29:23 +0300

    up

--- collector.py
+++ collector.py
@@ -1,5 +1,4 @@
 import time
-
 def update_helper4(to_dict,to_field,from_dict,from_field):
 	if from_field in from_dict:
 		to_dict[to_field]=from_dict[from_field]
@@ -10,3 +9,48 @@ def update_helper4(to_dict,to_field,from_dict,from_field):
 def update_helper(to_dict,from_dict,field):
 	update_helper4(to_dict,field,from_dict,field)
 	pass
+
+def update_history(to_dict,to_field,from_dict,from_field,period_sec):
+	ts=int(time.time())
+	history_data=[]
+	if to_field in to_dict:
+		oldest = ts - period_sec
+		for el in to_dict[to_field]:
+			if(el['ts']>=oldest):
+				history_data.append(el)
+				pass
+			pass
+		pass
+	if from_field in from_dict:
+		history_data.append({'val':from_dict[from_field],'ts':ts})
+		pass
+	to_dict[to_field]=history_data
+	pass
+
+def history_pack(from_dict,from_field,period_sec,step_sec):
+	oldest=int(time.time())-period_sec
+	tmp=dict()
+	if from_field in from_dict:
+		for el in from_dict[from_field]:
+			if el['ts']>=oldest:
+				index=int(el['ts']/step_sec)
+				if index not in tmp:
+					tmp[index]=[]
+					pass
+				tmp[index].append(el['val'])
+				pass
+			pass
+		pass
+	print("sorted", tmp)
+	values=[]
+	for key in tmp:
+		summ=0
+		accum=tmp[key]
+		for val in accum:
+			summ=summ+val
+			pass
+		values.append(summ/len(accum))
+		pass
+	result={'period_sec':period_sec,'step_sec':step_sec,'values':values}
+	print( "values:", result)
+	return result
--- static/index.js
+++ static/index.js
@@ -1,149 +1,57 @@
-var temperatureHistoryDiv = document.getElementById("temperature-history");
+const today = new Date();
+
 var humidityHistoryDiv = document.getElementById("humidity-history");
+var graphConfig = { displayModeBar: false, responsive: true,};
 
-var graphConfig = {
-  displayModeBar: false,
-  responsive: true,
-};
-
-// History Data
-var temperatureTrace = {
-  x: [],
-  y: [],
-  name: "Temperature",
-  mode: "lines+markers",
-  type: "line",
-};
-var humidityTrace = {
-  x: [],
-  y: [],
-  name: "Humidity",
-  mode: "lines+markers",
-  type: "line",
-};
-
-var temperatureLayout = {
-  autosize: true,
-  title: {
-    text: "Temperature",
-  },
-  font: {
-    size: 14,
-    color: "#7f7f7f",
-  },
-  colorway: ["#B22222"],
-  //   width: 450,
-  //   height: 260,
-  margin: { t: 30, b: 20, l: 30, r: 20, pad: 0 },
-};
-var humidityLayout = {
-  autosize: true,
-  title: {
-    text: "Humidity",
-  },
-  font: {
-    size: 14,
-    color: "#7f7f7f",
-  },
-  colorway: ["#00008B"],
-  //   width: 450,
-  //   height: 260,
-  margin: { t: 30, b: 20, l: 30, r: 20, pad: 0 },
-};
-var config = { responsive: true };
-
-Plotly.newPlot(
-  temperatureHistoryDiv,
-  [temperatureTrace],
-  temperatureLayout,
-  graphConfig
-);
-Plotly.newPlot(
-  humidityHistoryDiv,
-  [humidityTrace],
-  humidityLayout,
-  graphConfig
-);
-
-// Gauge Data
-var temperatureData = [
-  {
-    domain: { x: [0, 1], y: [0, 1] },
-    value: 0,
-    title: { text: "Temperature" },
-    type: "indicator",
-    mode: "gauge+number+delta",
-    delta: { reference: 30 },
-    gauge: {
-      axis: { range: [null, 50] },
-      steps: [
-        { range: [0, 20], color: "lightgray" },
-        { range: [20, 30], color: "gray" },
-      ],
-      threshold: {
-        line: { color: "red", width: 4 },
-        thickness: 0.75,
-        value: 30,
+function temperature_history(history){
+    var temperatureHistoryDiv = document.getElementById("temperature-history");
+    var temperatureLayout = {
+      title: {
+        text: "Temperature",
       },
-    },
-  },
-];
-
-var humidityData = [
-  {
-    domain: { x: [0, 1], y: [0, 1] },
-    value: 0,
-    title: { text: "Humidity" },
-    type: "indicator",
-    mode: "gauge+number+delta",
-    delta: { reference: 50 },
-    gauge: {
-      axis: { range: [null, 100] },
-      steps: [
-        { range: [0, 20], color: "lightgray" },
-        { range: [20, 30], color: "gray" },
-      ],
-      threshold: {
-        line: { color: "red", width: 4 },
-        thickness: 0.75,
-        value: 30,
+      font: {
+        size: 14,
+        color: "#7f7f7f",
       },
-    },
-  },
-];
+      colorway: ['#000000', '#808080'],
+      margin: { t: 30, b: 20, l: 30, r: 20, pad: 0 },
+      yaxis: {
+        autorange: true,
+      },
+      xaxis: {
+        autorange: false,
+        range: [0, 23],
+        type: 'linear'
+      },
+      showlegend:false,
+    };
 
-var layout = { width: 350, height: 250, margin: { t: 0, b: 0, l: 0, r: 0 } };
+    var hour=today.getHours();
+    var it=0
+    var data_today={mode:'lines+markers'}
+    data_today.x=[]
+    data_today.y=[]
+    var temperatures=history.values
+    while(it<temperatures.length && hour>=0){
+        data_today.x.push(hour)
+        data_today.y.push(temperatures[it])
+        hour--
+        it++
+    }
 
-// Temperature
-let newTempXArray = [];
-let newTempYArray = [];
-// Humidity
-let newHumidityXArray = [];
-let newHumidityYArray = [];
-
-// The maximum number of data points displayed on our scatter/line graph
-let MAX_GRAPH_POINTS = 12;
-let ctr = 0;
-
-function updateCharts(lineChartDiv, xArray, yArray, sensorRead) {
-  var today = new Date();
-  var time =
-    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  if (xArray.length >= MAX_GRAPH_POINTS) {
-    xArray.shift();
-  }
-  if (yArray.length >= MAX_GRAPH_POINTS) {
-    yArray.shift();
-  }
-  xArray.push(ctr++);
-  yArray.push(sensorRead);
-
-  var data_update = {
-    x: [xArray],
-    y: [yArray],
-  };
-
-  Plotly.update(lineChartDiv, data_update);
+    var data_yesterday={mode:"lines"}
+    data_yesterday.x=[]
+    data_yesterday.y=[]
+    hour=23
+    while(it<temperatures.length && hour>=0){
+        data_yesterday.x.push(hour)
+        data_yesterday.y.push(temperatures[it])
+        hour--
+        it++
+    }
+    console.log(data_today)
+    console.log(data_yesterday)
+    Plotly.newPlot( temperatureHistoryDiv,  [data_today, data_yesterday],  temperatureLayout,  graphConfig);
 }
 
 function updateWeatherData(weather) {
@@ -154,21 +62,9 @@ function updateWeatherData(weather) {
   $("#lighting").html(weather.lighting.toFixed(0) + " Lux")
   $("#battery").html(weather.battery.toFixed(2)+" V" )
   $("#rssi").html(weather.rssi)
-
-  // Update Temperature Line Chart
-  updateCharts(
-    temperatureHistoryDiv,
-    newTempXArray,
-    newTempYArray,
-    temperature
-  );
-  // Update Humidity Line Chart
-  updateCharts(
-    humidityHistoryDiv,
-    newHumidityXArray,
-    newHumidityYArray,
-    humidity
-  );
+  if (weather.history_temperature){
+    temperature_history(weather.history_temperature)
+  }
 }
 /*
   SocketIO Code
