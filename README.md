0af1ea9db3e828f35ae838993dfe46ffc245bf2c
Oleksandr Minenko <oleksandr.minenko@ringteam.com>
2023-08-13 22:43:12 +0300

    upd

--- README.md
+++ README.md
@@ -1,28 +1,16 @@
 # Raspberry Pi DHT22 Weather Station Python Flask Socketio  
 A DHT Weather Station project using Python, Flask, Flask-SocketIO, and Bootstrap that displays real-time temperature and humidity sensor readings using your Raspberry Pi
-![Featured Image - DHT22 Weather Station With Python, Flask, Flask-SocketIO](https://user-images.githubusercontent.com/69466026/236615315-5a7bba1c-9712-48bd-8718-29039850d655.jpg)  
   
 ## Writeup
 https://www.donskytech.com/raspberry-pi-dht22-weather-station-project/
 
-## Prerequisites
-Make sure to install the DHT22 driver on your Raspberry Pi using the Adafruit_circuitPython_dht library.   
-Follow the following post https://www.donskytech.com/raspberry-pi-how-to-interface-with-a-dht22-sensor/
-  
-## Steps on how to run on Raspberry Pi
-1. Clone the repository
-```
-git clone https://github.com/donskytech/dht22-weather-station-python-flask-socketio.git
-cd dht22-weather-station-python-flask-socketio
-```
-2. Create a Python virtual environment
-```
 python -m venv .venv
 source .venv/bin/activate
 ```
 3. Install the dependencies
 ```
 pip install -r requirements.txt
+cp config_.yaml config.yaml
 ```
 
 4. Run the application
--- tbot_module.py
+++ tbot_module.py
@@ -1,9 +1,67 @@
-
 import telebot
 
 class TBot:
-    def __init__(self,token):
-        self.tbot=telebot.TeleBot("TOKEN")
+    def __init__(self):
+        class cmd_helper:
+            def __init__(self,description,handler,scope):
+                self.description=description
+                self.handler = handler
+                self.scope = scope
+                pass
+        self.__cmds={}
+        self.__cmds['status'] = cmd_helper("стан системи",self.__cmd_status,['menu'])
+        self.__cmds['help'] = cmd_helper("допомога", self.__cmd_help, ['menu'])
+        pass
+    def __cmd_status(self,message):
+        print(message)
+        self.__tbot.reply_to(message.chat.id, '__cmd_status')
+        pass
+    def __cmd_help(self,message):
+        help="доступні команди"
+        for cmd,data in self.__cmds.items():
+            help+=f"\n/{cmd} {data.description}"
+            pass
+        self.__tbot.reply_to(message, help)
+        pass
+    def __handle_messages(self,messages):
+        for message in messages:
+            print(message)
+            self.__tbot.reply_to(message, 'Hi')
+            pass
+        pass
+
+    def __message_handler(self,message):
+        print(message)
+        self.__tbot.reply_to(message.chat.id, 'Hi')
+        pass
+    def start(self,token):
+        self.__tbot=telebot.TeleBot(token)
+        self.__tbot.delete_my_commands()
+        menu_commands=[]
+        for cmd,data in self.__cmds.items():
+            if 'menu' in data.scope:
+                menu_commands.append(telebot.types.BotCommand(cmd,data.description))
+                pass
+            self.__tbot.register_message_handler(data.handler, commands=[cmd])
+            pass
+        self.__tbot.set_my_commands(
+            commands=menu_commands,
+        )
+
+        #self.__tbot.set_update_listener(self.__handle_messages)
+        pass
+
+    def run(self):
+        self.__tbot.infinity_polling()
         pass
     pass
 
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    tbot=TBot()
+    tbot.start(sys.argv[1])
+    tbot.run()
