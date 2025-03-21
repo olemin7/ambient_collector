cb0ae6663fcdc6d6a66a646e21ef188a9c5f53e2
Oleksandr Minenko <oleksandr.minenko@squad.tech>
2025-03-21 12:29:02 +0200

    upd

--- static/things.js
+++ static/things.js
@@ -19,7 +19,6 @@ function add_to_tree_(name,value){
       instance.hide_icon(name);
     }
     instance.rename_node(name, keys.at(-1)+" : "+value);
-  //   console.log(instance.get_node(""));
 }
 
 
@@ -33,6 +32,51 @@ function update_value(name,value){
     }
 }
 
+
+var graphConfig = { displayModeBar: false,staticPlot: false , autoscaleYAxis: true};
+
+function history(parameters){
+    const place_holder_div = document.getElementById("id_graphs");
+    var layout = {
+      font: {
+        size: 14,
+        color: "#7f7f7f",
+      },
+      colorway: ['#000000', '#808080'],
+      margin: { t: 30, b: 20, l: 30, r: 20, pad: 0 },
+      yaxis: {
+        autorange: true,
+        title: {
+            text: name,
+        },
+      },
+      xaxis: {
+        autorange: true,
+        type: 'date'
+        },
+      showlegend:true,
+    };
+
+    Plotly.newPlot( place_holder_div, [],  layout,  graphConfig);
+    parameters.forEach((key) => {
+        socket.emit("history",{key: key}, (response) => {
+            if(response!=undefined){
+                var data={
+                    name: key,
+                    mode:'lines',
+                    x:[],
+                    y:[]
+                }
+                 response.forEach((element) => {
+                    data.x.push(ts_to_date(element.ts))
+                    data.y.push(element.value)
+                });
+                Plotly.addTraces( place_holder_div, [data]);
+            }
+        });
+     } );
+}
+
 function page_start_up(){
     $('#tree_id').jstree({ 'core' : {
         "check_callback": true,
@@ -40,7 +84,7 @@ function page_start_up(){
         "plugins" : [ "sort" ],
     } });
     $('#tree_id').on("changed.jstree", function (e, data) {
-      console.log(data.selected);
+      console.log(data.selected); ///todo optimasation
+      history(data.selected);
     });
-
 }
